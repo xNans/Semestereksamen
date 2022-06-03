b9bd23156f0a2f56acca89287470e65548e68b48
xNans <nannamc@live.dk>
2022-06-03 07:25:36 +0200

    .

--- techhubsyd/header.php
+++ techhubsyd/header.php
@@ -7,4 +7,10 @@
     <title>Techhub Syd</title>
     <link rel='stylesheet' href='<?php echo get_stylesheet_directory_uri() ?>/style.css'/>
     
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Oxanium:wght@200;300;400;500;600;700;800&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet"> 
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Oxanium:wght@200;300;400;500;600;700;800&family=Roboto+Condensed:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap" rel="stylesheet">
 </head>
